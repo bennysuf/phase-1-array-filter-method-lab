e53622c8e653cec3acbf04bb898f510061ba3f94
Benny Sufrin <bennysuf@gmail.com>
2022-08-21 13:07:40 -0400

    commit tree 8a7676ccb08713b5983b6296b22406befa1fef0e

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+
+function findMatching (arr, string) {
+ return arr.filter(function (matchingString){
+   return matchingString.toUpperCase() === string.toUpperCase()
+ })
+}
+
+
+function fuzzyMatch(arr, letters) {
+  return arr.filter(function (names) {
+    return names.startsWith(letters) 
+  })
+}
+
+function matchName(arr, string) {
+   return arr.filter(function (matchingString) {
+     return matchingString["name"] === string 
+    })
+}
